00f04eedf4ca4c6fee7a15ad609599f4d34d2834
Hillary <Hillaryd97@gmail.com>
2023-09-28 10:26:34 +0100

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { MdEmail } from "react-icons/md";
 const Footer = () => {
   return (
     <div className="bg-black">
-      <div className="container mx-auto py-6 lg:flex space-y-3 lg:space-y-0 lg:space-x-4 bg-black lg:justify-between justify-center text-white">
+      <div className="container mx-auto py-6 lg:flex space-y-3 lg:space-y-0 lg:space-x-4 bg-black lg:justify-evenly justify-center text-white">
         <div className="flex flex-col items-center mr-8 lg lg:flex-row">
           <div className="flex items-center space-x-1">
             <img src={logo} alt="" className="w-16 " />
@@ -17,10 +17,10 @@ const Footer = () => {
         <div className="flex flex-col lg:items-start text-sm items-center lg:max-w-1/4">
           <div className="text-center lg:text-left">
             <h5 className="font-playfair-display font-semibold text-base text-secondary">
-              Accepted Payment Methods:
+              Accepted Payment Method
             </h5>
             <div className="mt-2">
-              <p>
+              {/* <p>
                 - <span className="font-bold">Name:</span> Ruby Integrated
                 Global Services Limited
               </p>
@@ -30,7 +30,8 @@ const Footer = () => {
                 0013026916 (Jaiz bank)
                 <br />
                 0037940253 (Stanbic IBTC)
-              </p>
+              </p> */}
+              <p>Bank Transfer</p>
             </div>
           </div>
         </div>
@@ -42,11 +43,11 @@ const Footer = () => {
           <p>Bread</p>
           <p>Outdoor Events</p>
         </div>
-        <div className="flex flex-col lg:items-start text-sm items-center lg:max-w-1/4">
+        <div className="flex flex-col lg:items-start text-sm items-center px-2 lg: lg:w-72 lg:max-w-1/4">
           <h5 className="font-playfair-display font-semibold text-base text-secondary">
             Shop Location
           </h5>
-          <div className="flex space-x-2 items-center text-center px-2">
+          <div className="flex space-x-2 lg:space-x-0 items-center text-center lg:px-0 px-2">
             <address className="text-justify">
               <span className="font-bold"> Nasarawa </span> - Ruby Bakery, No.
               10, Alloyd Monek International School Avenue, Karu Local
